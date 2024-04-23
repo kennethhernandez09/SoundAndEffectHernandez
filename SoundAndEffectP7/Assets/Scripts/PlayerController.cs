bb6e546377239997001bcdc898342ab9e1be9bea
Kenneth Hernandez <145072274+kennethhernandez09@users.noreply.github.com>
2024-04-22 22:47:43 -0700

    Did half of project
    On Lesson 3.3
    -Bug on obstacles respawning and getting destroy
    (FIX IMMEDIATELY)

---
+++ SoundAndEffectP7/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+    public bool gameOver = false;
+    private Animator playerAnim;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    public bool isOnGround = true;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) 
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+        }
+    }
+}
